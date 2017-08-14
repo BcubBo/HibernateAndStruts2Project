4605eff538627d2bf010f1daf518b51d18aaea56
bowieu <fatalitywolf@foxmail.com>
2017-08-14 10:08:08 +0800

    添加entity

---
+++ src/entity/DocEntry.java
@@ -0,0 +1,53 @@
+package entity;
+
+import java.util.Date;
+
+public class DocEntry {
+	
+	private int id;
+	private int categoryid;
+	private String title;
+	private String summary;
+	private String uploaduser;
+	private Date createdate;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getCategoryid() {
+		return categoryid;
+	}
+	public void setCategoryid(int categoryid) {
+		this.categoryid = categoryid;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getSummary() {
+		return summary;
+	}
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+	public String getUploaduser() {
+		return uploaduser;
+	}
+	public void setUploaduser(String uploaduser) {
+		this.uploaduser = uploaduser;
+	}
+	public Date getCreatedate() {
+		return createdate;
+	}
+	public void setCreatedate(Date createdate) {
+		this.createdate = createdate;
+	}
+	
+	
+	
+	
+}
