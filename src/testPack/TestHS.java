949c13246217e010f710e1dfdb6ad4cf5dc4a115
bowieu <fatalitywolf@foxmail.com>
2017-08-14 13:33:17 +0800

    cascade级联操作的重要性

--- src/testPack/TestHS.java
+++ src/testPack/TestHS.java
@@ -1,15 +1,22 @@
 package testPack;
 
+import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import entity.EDoc_Category;
 import entity.EDoc_Entry;
 import util.HibernateSessionFactory;
 
 public class TestHS {
-	
+
 	public static void main(String[] args) {
+		Session session = HibernateSessionFactory.getSession();
+		Transaction ts = session.getTransaction();
 		@SuppressWarnings("unchecked")
 		List<EDoc_Category> ecList = HibernateSessionFactory.getSession().createQuery("from EDoc_Category ec where ec.id = 1").list();
 		for(EDoc_Category ec:ecList) {
@@ -25,6 +32,36 @@ public class TestHS {
 			System.out.println("结果:"+ee);
 			
 		}
+
+		try {
+			
+			ts.begin();
+			EDoc_Entry ee1 = new EDoc_Entry();
+			Set<EDoc_Entry> eeSet = new HashSet<EDoc_Entry>();
+			ee1.setCategoryid(1);
+			ee1.setCreatedate(new Date());
+			ee1.setTitle("testTitle");
+			eeSet.add(ee1);
+			EDoc_Category ec1 = new EDoc_Category();
+			ec1.setDocEntrys(eeSet);
+			ec1.setName("test1");
+			ee1.setDocCategory(ec1);
+			session.save(ec1);
+			ts.commit();
+			
+			
+			
+		}catch(Exception e) {
+			if(session!=null) {
+				
+				ts.rollback();
+				
+				
+			}
+			
+			e.printStackTrace();
+		}
+		
 	}
 	
 	
